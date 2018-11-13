919062b95e670c98aecccd10b305e0d33fb57f9b
SomeStudentLOL404 <44593734+SomeStudentLOL404@users.noreply.github.com>
2018-11-12 22:15:31 -0500

    Beginning - 6/10
    Starting off at this - Starting at 6/10 from P1
    
    SUMMARY:
    RESULT: compiles [ 1 / 1 ]
    RESULT: good1 [  0  / 1 ]
    RESULT: good2 [  0  / 1 ]
    RESULT: good3 [  0  / 1 ]
    RESULT: good4 [  0  / 1 ]
    RESULT: bad1 [  1  / 1 ]
    RESULT: bad2 [  1  / 1 ]
    RESULT: bad3 [  1  / 1 ]
    RESULT: bad4 [  1  / 1 ]
    RESULT: bad5 [  1  / 1 ]
    TOTAL SCORE 6 out of 10

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include "tokens.h"
+#include "parse.h"
+#include "lex.h"
+
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+    ifstream infile1;
+	istream *in = &cin;
+	int linenum = 0;
+    
+    for(int i = 1; i < argc; i++)
+    {       
+            string arg(argv[i]);
+            if(in != &cin)
+            {
+                cout << "TOO MANY FILENAMES" << endl;
+                return -1;
+            }
+            else {
+                infile1.open(arg);
+                if (infile1.is_open() == false)
+                {
+                    cout << "COULD NOT OPEN " << arg << endl;
+                    return -1;
+                }
+                in = &infile1;
+            }
+     }
+    ParseTree *prog = Prog(in, &linenum);
+    if( prog == 0 )
+    {
+        return 0; // quit on error
+    }
+
+    return 0;
+}
